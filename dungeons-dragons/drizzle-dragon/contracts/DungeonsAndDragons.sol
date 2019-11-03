312161097e085ad477c592e77ef41aa7da735667
Fede GB <fedeagb@gmail.com>
2019-11-03 20:23:29 -0300

    Added new methods and test

--- dungeons-dragons/drizzle-dragon/contracts/DungeonsAndDragons.sol
+++ dungeons-dragons/drizzle-dragon/contracts/DungeonsAndDragons.sol
@@ -7,33 +7,6 @@ contract DungeonsAndDragons {
 
 	mapping (address => int) public balances;
 
-/*	enum race {
-		HALFELF,
-		HALFORC,
-		HUMAN,
-		DWARF,
-		GNOME,
-		ELF,
-		HALFLING,
-		DRAGONBORN,
-		TIEFLING
-	}
-
-    enum class {
-		DRUID,
-		ROGUE,
-		FIGHTER,
-		WARLOCK,
-		BARBARIAN,
-		BARD,
-		CLERIC,
-		MONK,
-		PALADIN,
-		RANGER,
-		SORCERER,
-		WIZARD
-	}
-*/
 	struct BaiscCharacterRecordSheet {
 		// address characterBlockDir;
 		string name;
@@ -45,7 +18,24 @@ contract DungeonsAndDragons {
 		string gender;
 	}
 
+	struct StatsCharacterRecordSheet {
+		uint64 strength;
+		int64 strengthModifier;
+		uint64 dexterity;
+		int64 dexterityModifier;
+		uint64 constitution;
+		int64 constitutionModifier;
+		uint64 wisdom;
+		int64 wisdomModifier;
+		uint64 intelligence;
+		int64 intelligenceModifier;
+		uint64 charisma;
+		int64 charismaModifier;
+	}
+
 	BaiscCharacterRecordSheet public playerSheet;
+
+	StatsCharacterRecordSheet public playerSheetStats;
 
 	constructor() public payable {
 	    creator = msg.sender;
@@ -55,7 +45,51 @@ contract DungeonsAndDragons {
 
 	function setBasicCharacterSheet(string memory name, string memory player, string memory class, uint64 level, string memory race, uint64 age, string memory gender) public {
 		require(msg.sender == creator, "Only the creator of it\'s own player sheet can modify it\'s data");
-		playerSheet = BaiscCharacterRecordSheet({name: name, player: player, class: class, level: level, race: race, age: age, gender: gender});
+		playerSheet = BaiscCharacterRecordSheet({
+			name: name,
+			player: player,
+			class: class,
+			level: level,
+			race: race,
+			age: age,
+			gender: gender
+		});
+	}
+
+	function getStatModifier(uint64 stat) internal pure returns(int64) {
+		if(stat == 3) return -4;
+		if(stat > 3 && stat <= 5) return -3;
+		if(stat > 5 && stat <= 7) return -2;
+		if(stat > 7 && stat <= 9) return -1;
+		if(stat > 9 && stat <= 11) return 0;
+		if(stat > 11 && stat <= 13) return 1;
+		if(stat > 13 && stat <= 15) return 2;
+		if(stat > 15 && stat <= 17) return 3;
+		if(stat == 18) return 4;
+	}
+
+	function setStatsCharacterSheet(uint64 strength, uint64 dexterity, uint64 constitution, uint64 wisdom, uint64 intelligence, uint64 charisma) public {
+		require(msg.sender == creator, "Only the creator of it\'s own player sheet can modify it\'s data");
+		int64 strengthModifier = getStatModifier(strength);
+		int64 dexterityModifier = getStatModifier(dexterity);
+		int64 constitutionModifier = getStatModifier(constitution);
+		int64 wisdomModifier = getStatModifier(wisdom);
+		int64 intelligenceModifier = getStatModifier(intelligence);
+		int64 charismaModifier = getStatModifier(charisma);
+		playerSheetStats = StatsCharacterRecordSheet({
+			strength: strength,
+		 	strengthModifier: strengthModifier,
+		 	dexterity: dexterity,
+		 	dexterityModifier: dexterityModifier,
+		 	constitution: constitution,
+		 	constitutionModifier: constitutionModifier,
+		 	wisdom: wisdom,
+		 	wisdomModifier: wisdomModifier,
+		 	intelligence: intelligence,
+		 	intelligenceModifier: intelligenceModifier,
+		 	charisma: charisma,
+		 	charismaModifier: charismaModifier
+		 });
 	}
 
 }
--- dungeons-dragons/drizzle-dragon/test/DungeonsAndDragons.js
+++ dungeons-dragons/drizzle-dragon/test/DungeonsAndDragons.js
@@ -12,4 +12,22 @@ contract("DungeonsAndDragons", accounts => {
 	assert.equal(playerSheet.age, 27);
 	assert.equal(playerSheet.gender, "Male");
   });
+
+  it("should match stats player sheet", async () => {
+    const dyd = await DungeonsAndDragons.deployed();
+    await dyd.setStatsCharacterSheet(4, 6, 8, 10, 12, 18);
+    const playerSheetStats = await dyd.playerSheetStats.call();
+	assert.equal(playerSheetStats.strength, 4);
+	assert.equal(playerSheetStats.strengthModifier, -3);
+	assert.equal(playerSheetStats.dexterity, 6);
+	assert.equal(playerSheetStats.dexterityModifier, -2);
+	assert.equal(playerSheetStats.constitution, 8);
+	assert.equal(playerSheetStats.constitutionModifier, -1)
+	assert.equal(playerSheetStats.wisdom, 10);
+	assert.equal(playerSheetStats.wisdomModifier, 0);
+	assert.equal(playerSheetStats.intelligence, 12);
+	assert.equal(playerSheetStats.intelligenceModifier, 1);
+	assert.equal(playerSheetStats.charisma, 18);
+	assert.equal(playerSheetStats.charismaModifier, 4);
+  });
 });
